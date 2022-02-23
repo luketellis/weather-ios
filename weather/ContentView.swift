a500b787e55e4b2e46089e2687c0d1deed89152d
luke <luketellis@hotmail.com>
2022-02-23 15:24:38 +1100

    Initial Commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by Luke Tellis on 23/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Luke Tellis on 23/2/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
